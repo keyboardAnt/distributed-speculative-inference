4bfd8dff685ec692397e0b7516e752bd0fa40ef7
Nadav Timor <nadavtim@mit.edu>
2024-06-10 20:55:11 -0400

    Analytic SI (#7)
    implement an analytic simulation of SI

--- dsi/main.py
+++ dsi/main.py
@@ -1,11 +1,37 @@
 """The main entrypoint for the CLI."""
 
+import os
+import hydra
+from matplotlib import pyplot as plt
+from omegaconf import OmegaConf
 
-class SimulationMode(str):
-    analytic = "analytic"
-    thread_pool = "thread_pool"
+from dsi.config import Config, SimulationType
+from dsi.analytic.si import RunSI
+import logging
+
+from dsi.schemas.results import ResultSI
+from dsi.vis.iters_dist import PlotIters
+
+log = logging.getLogger(__name__)
 
 
-def main(simulation_mode: SimulationMode):
-    print(f"{simulation_mode=}")
-    print("Starting...")
+@hydra.main(version_base=None, config_name="config")
+def main(cfg: Config) -> None:
+    log.info(OmegaConf.to_yaml(cfg))
+    log.info("Working directory: %s", os.getcwd())
+    log.info(
+        "Output directory: %s",
+        hydra.core.hydra_config.HydraConfig.get().runtime.output_dir,
+    )
+    if cfg.simulation_type == SimulationType.analytic:
+        res_si: ResultSI = RunSI(cfg.config_run).run()
+        plot_static: PlotIters = PlotIters(
+            result=res_si, suptitle=f"Latency of SI (lookahead={cfg.config_run.k})"
+        )
+        plot_static.plot()
+    elif cfg.simulation_type == SimulationType.thread_pool:
+        raise NotImplementedError
+    else:
+        raise ValueError(f"Invalid simulation type: {cfg.simulation_type}")
+    plt.show()
+    log.info("Done")
---
+++ dsi/vis/cost_dist.py
@@ -0,0 +1,45 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+
+from dsi.schemas.run import Result
+
+
+class PlotCost:
+    def __init__(self, result: Result, suptitle: str | None = None) -> None:
+        self.result: Result = result
+        self._suptitle: str | None = suptitle
+        fig, axes = self._get_axes()
+        self._fig: plt.Figure = fig
+        self._axes: list[plt.Axes] = axes
+
+    def plot(self) -> None:
+        """
+        Plots the total latency distribution and the number of iterations.
+        """
+        if self._suptitle:
+            self._fig.suptitle(self._suptitle)
+        self._plot_total_costs(self._axes[0])
+
+    def _get_axes(self) -> tuple[Figure, list[plt.Axes]]:
+        fig: plt.Figure
+        ax: plt.Axes
+        fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+        return fig, [ax]
+
+    def _plot_total_costs(self, ax: plt.Axes) -> None:
+        ax.hist(
+            self.result.cost_per_run,
+            bins=30,
+            density=True,
+            color="purple",
+            alpha=0.7,
+        )
+        self._plot_mean(ax=ax, val=np.mean(self.result.cost_per_run))
+        ax.set_xlabel("Total Latency")
+        ax.set_ylabel("Frequency")
+        ax.set_title("Total Latency Distribution")
+
+    def _plot_mean(self, ax: plt.Axes, val: float) -> None:
+        ax.axvline(x=val, color="black", linestyle="--")
+        ax.text(val, 0.02, f"Mean: {val:.2f}", color="black", fontsize=8)
