65b25cd53803b2c5a33bcd25e9a905d35332ed3d
Nadav Timor <nadavtim@mit.edu>
2024-06-25 16:33:50 -0400

    fix

--- dsi/schemas/results.py
+++ dsi/schemas/results.py
@@ -10,11 +10,9 @@ class ResultSI(Result):
     """
     In addition to the base result:
     Args:
-        cost_per_run: The total latency for each run
         num_iters_per_run: The number of iterations for each run
     """
 
-    cost_per_run: list[float] = field(default_factory=list)
     num_iters_per_run: list[int] = field(default_factory=list)
 
 
