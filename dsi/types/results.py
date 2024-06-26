c2726719ecdb3ad1e02176cdc6a36e6ff75d65bd
Nadav Timor <nadavtim@mit.edu>
2024-06-26 12:46:50 -0400

    add `ResultSIVerbose` with a reference to the TODO github issue

--- dsi/types/results.py
+++ dsi/types/results.py
@@ -22,3 +22,18 @@ class ResultSI(Result):
     """
 
     num_iters_per_run: list[int] = field(default_factory=list)
+
+
+@dataclass
+class ResultSIVerbose(Result):
+    """
+    Extends ResultSI with the following:
+    Args:
+        num_toks_of_last_iter_per_run: The number of tokens in the last iteration for each run
+        num_toks_per_iter: The number of tokens in each iteration, across all runs
+
+    TODO(#13).
+    """
+
+    num_toks_of_last_iter_per_run: list[int] = field(default_factory=list)
+    num_toks_per_iter: list[int] = field(default_factory=list)
