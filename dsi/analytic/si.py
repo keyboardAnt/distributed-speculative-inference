7f7cc9f5ccfb9cb174f3c87c21b068242015ab72
Nadav Timor <nadavtim@mit.edu>
2024-06-29 13:18:06 -0400

    `Run. _run_single`

--- dsi/analytic/si.py
+++ dsi/analytic/si.py
@@ -9,18 +9,34 @@ class RunSI(Run):
     def _get_empty_result(self) -> ResultSI:
         return ResultSI()
 
-    def run(self) -> ResultSI:
-        for _ in range(self.config.num_repeats):
-            total_cost: float = 0
-            total_toks: int = 0
-            num_iters: int = 0
-            while total_toks < self.config.S:
-                num_accepted: int = get_num_accepted_tokens(
-                    acceptance_rate=self.config.a, lookahead=self.config.k
-                )
-                total_toks += num_accepted + 1
-                total_cost += self.config.k * self.config.c + self.config.failure_cost
-                num_iters += 1
-            self.result.cost_per_run.append(total_cost)
-            self.result.num_iters_per_run.append(num_iters)
-        return self.result
+    # def run(self) -> ResultSI:
+    #     for _ in range(self.config.num_repeats):
+    #         total_cost: float = 0
+    #         total_toks: int = 0
+    #         num_iters: int = 0
+    #         while total_toks < self.config.S:
+    #             num_accepted: int = get_num_accepted_tokens(
+    #                 acceptance_rate=self.config.a, lookahead=self.config.k
+    #             )
+    #             total_toks += num_accepted + 1
+    #             total_cost += self.config.k * self.config.c + self.config.failure_cost
+    #             num_iters += 1
+    #         self.result.cost_per_run.append(total_cost)
+    #         self.result.num_iters_per_run.append(num_iters)
+    #     return self.result
+
+    def _run_single(self) -> ResultSI:
+        result = ResultSI()
+        total_cost: float = 0
+        total_toks: int = 0
+        num_iters: int = 0
+        while total_toks < self.config.S:
+            num_accepted: int = get_num_accepted_tokens(
+                acceptance_rate=self.config.a, lookahead=self.config.k
+            )
+            total_toks += num_accepted + 1
+            total_cost += self.config.k * self.config.c + self.config.failure_cost
+            num_iters += 1
+        result.cost_per_run = [total_cost]
+        result.num_iters_per_run = [num_iters]
+        return result
--- dsi/types/run.py
+++ dsi/types/run.py
@@ -1,3 +1,5 @@
+from typing import final
+
 from dsi.configs.config_run import ConfigRun
 from dsi.types.result import Result
 from dsi.utils import set_random_seed
@@ -12,5 +14,11 @@ class Run:
     def _get_empty_result(self) -> Result:
         return Result()
 
+    @final
     def run(self) -> Result:
+        for _ in range(self.config.num_repeats):
+            self.result.extend(self._run_single())
+        return self.result
+
+    def _run_single(self) -> Result:
         raise NotImplementedError
