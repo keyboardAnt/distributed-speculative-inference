8a823e7d842a3fdc4f1f16e2a07b44530a2ecd34
Nadav Timor <nadavtim@mit.edu>
2024-06-30 10:35:59 -0400

    main: run dsi & log

--- dsi/configs/config_cli.py
+++ dsi/configs/config_cli.py
@@ -4,7 +4,7 @@ from hydra.core.config_store import ConfigStore
 from pydantic import BaseModel, Field
 
 from dsi.configs.config_heatmap import ConfigHeatmap
-from dsi.configs.config_run import ConfigRun
+from dsi.configs.config_run import ConfigRun, ConfigRunDSI
 
 
 class RunType(str, enum.Enum):
@@ -15,7 +15,7 @@ class RunType(str, enum.Enum):
 
 class ConfigCLI(BaseModel):
     run_type: RunType = RunType.analytic
-    config_run: ConfigRun | ConfigHeatmap = Field(default_factory=ConfigRun)
+    config_run: ConfigRunDSI | ConfigHeatmap = Field(default_factory=ConfigRunDSI)
 
     def model_post_init(self, __context) -> None:
         super().model_post_init(__context)
