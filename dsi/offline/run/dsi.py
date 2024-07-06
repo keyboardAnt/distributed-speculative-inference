a0710e69b5c77af8b3469c3ff083aaf5380d96ae
Nadav Timor <nadavtim@mit.edu>
2024-07-06 17:08:08 -0400

    remove commented out code

--- dsi/offline/run/dsi.py
+++ dsi/offline/run/dsi.py
@@ -14,32 +14,6 @@ class RunDSI(Run):
         """
         super().__init__(config)
 
-    # def _run_single(self) -> Result:
-    #     total_cost: float = 0
-    #     toks_left: int = self.config.S
-    #     num_iters: int = 0
-    #     while toks_left > 0:
-    #         num_iters += 1
-    #         # if toks_left == 1:
-    #         #     total_cost += self.config.failure_cost
-    #         #     break
-    #         is_halting_feasible: bool = toks_left <= self.config.k + 1
-    #         curr_k: int = min(self.config.k, toks_left - 1)
-    #         num_accepted: int = min(curr_k, next(self._sampler))
-    #         toks_left -= num_accepted + 1
-    #         cost_priv: float = (num_accepted + 1) * self.config.failure_cost
-    #         cost_drafting: float = curr_k * self.config.c
-    #         if cost_priv <= cost_drafting:
-    #             total_cost += cost_priv
-    #         else:
-    #             total_cost += cost_drafting
-    #             if (num_accepted < self.config.k) or is_halting_feasible:
-    #                 self.config.failure_cost
-    #     return Result(
-    #         cost_per_run=[total_cost],
-    #         num_iters_per_run=[num_iters],
-    #     )
-
     def _run_single(self) -> Result:
         cost: float = 0
         toks: int = 0
