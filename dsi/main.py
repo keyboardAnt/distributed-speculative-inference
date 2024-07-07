7c0b006ba7a3f80582f9dfc0f3de0c047f8becff
Nadav Timor <nadavtim@mit.edu>
2024-07-07 19:10:58 -0400

    Update main.py

--- dsi/main.py
+++ dsi/main.py
@@ -34,13 +34,36 @@ def offline(cfg: ConfigCLI) -> None:
     log.info("Figure saved at %s", filepath_plots)
 
 
-def offline_heatmap(cfg: ConfigCLI) -> str:
+def offline_heatmap(cfg: ConfigCLI) -> None:
+    heatmap_filepath: None | str = cfg.load_csv
+    if cfg.load_csv is None:
+        log.info(
+            "Running a new experiment. Results will be stored at %s", heatmap_filepath
+        )
+    else:
+        log.info(
+            "Received a path to load existing heatmap results."
+            " Only visualizing the heatmap rather than running a new experiment."
+        )
+        log.info("Loading results from %s", heatmap_filepath)
     tmanager: RayManager = RayManager(cfg.heatmap)
     df_results: pd.DataFrame = tmanager.run()
     df_heatmap: DataFrameHeatmap = enrich_inplace(df_results)
     filepath: str = df_heatmap.store()
     log.info("Heatmap stored at %s", filepath)
-    return filepath
+    df_heatmap: DataFrameHeatmap = DataFrameHeatmap.from_heatmap_csv(cfg.load_csv)
+    log.info(f"{df_heatmap.shape=}")
+    log.info("df_heatmap.head():")
+    log.info(df_heatmap.head())
+    log.info("df_heatmap.describe():")
+    log.info(df_heatmap.describe())
+    log.info("Plotting heatmaps")
+    # # TODO(Nadav)
+    # log.info(f"{len(configs)=}")
+    # for config in configs:
+    #     log.info(f"Plotting speedup of {config=}")
+    #     filepath: str = plot_speedup(config)
+    #     log.info("Figure saved at %s", filepath)
 
 
 def online(cfg: ConfigCLI) -> None:
@@ -55,41 +78,19 @@ def main(cfg: ConfigCLI) -> None:
         "Output directory: %s",
         hydra.core.hydra_config.HydraConfig.get().runtime.output_dir,
     )
-    heatmap_filepath: None | str = cfg.load_csv
-    if cfg.load_csv is None:
-        log.info("Running a new experiment")
-        match cfg.type:
-            case RunType.offline:
-                log.info("Running offline simulations of SI and visualizing them.")
-                offline(cfg)
-            case RunType.offline_heatmap:
-                log.info("Running offline heatmap experiment.")
-                heatmap_filepath: str = offline_heatmap(cfg)
-            case RunType.online:
-                log.info(
-                    "Running online experiment."
-                    " Implementation with a thread pool to be added."
-                )
-                online(cfg)
-            case _:
-                raise NotImplementedError(f"Invalid simulation type: {cfg.type}")
-    else:
-        log.info(
-            "Received a path to load existing results."
-            " Visualizing them rather than running new experiments."
-        )
-        log.info("Loading results from %s", heatmap_filepath)
-    df_heatmap: DataFrameHeatmap = DataFrameHeatmap.from_heatmap_csv(cfg.load_csv)
-    log.info(f"{df_heatmap.shape=}")
-    log.info("df_heatmap.head():")
-    log.info(df_heatmap.head())
-    log.info("df_heatmap.describe():")
-    log.info(df_heatmap.describe())
-    log.info("Plotting heatmaps")
-    # TODO
-    # log.info(f"{len(configs)=}")
-    # for config in configs:
-    #     log.info(f"Plotting speedup of {config=}")
-    #     filepath: str = plot_speedup(config)
-    #     log.info("Figure saved at %s", filepath)
+    match cfg.type:
+        case RunType.offline:
+            log.info("Running offline simulations of SI and visualizing them.")
+            offline(cfg)
+        case RunType.offline_heatmap:
+            log.info("Running offline heatmap experiment.")
+            offline_heatmap(cfg)
+        case RunType.online:
+            log.info(
+                "Running online experiment."
+                " Implementation with a thread pool to be added."
+            )
+            online(cfg)
+        case _:
+            raise NotImplementedError(f"Invalid simulation type: {cfg.type}")
     log.info("Done")
