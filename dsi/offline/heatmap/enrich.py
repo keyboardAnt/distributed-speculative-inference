a6ddd22f5891a982d27fe350638a75cc6f53b456
Nadav Timor <nadavtim@mit.edu>
2024-07-12 10:49:33 -0400

    remove old `get_enriched_min_speedups` commented code

--- dsi/offline/heatmap/enrich.py
+++ dsi/offline/heatmap/enrich.py
@@ -4,34 +4,6 @@ import pandas as pd
 from dsi.types.df_heatmap import DataFrameHeatmap
 from dsi.types.name import HeatmapColumn, Param
 
-# def get_enriched_min_speedups(df: pd.DataFrame) -> pd.DataFrame:
-#     df = df.set_index([Param.c, Param.a])
-#     df[HeatmapColumn.cost_baseline] = np.minimum(
-#         df[HeatmapColumn.cost_si], df[HeatmapColumn.cost_nonsi]
-#     )
-#     # Calculate the minimum costs for each group
-#     min_cost_fed: pd.Series = df.groupby(level=[Param.c, Param.a])[
-#         HeatmapColumn.cost_dsi
-#     ].transform("min")
-#     min_cost_spec: pd.Series = df.groupby(level=[Param.c, Param.a])[
-#         HeatmapColumn.cost_si
-#     ].transform("min")
-#     min_cost_baseline: pd.Series = df.groupby(level=[Param.c, Param.a])[
-#         HeatmapColumn.cost_baseline
-#     ].transform("min")
-
-#     # Calculate speedups using the correctly aligned min values
-#     df[HeatmapColumn.min_speedup_dsi_vs_si] = min_cost_spec / min_cost_fed
-#     df[HeatmapColumn.min_speedup_dsi_vs_nonsi] = (
-#         df[HeatmapColumn.cost_nonsi] / min_cost_fed
-#     )
-#     df[HeatmapColumn.min_speedup_si_vs_nonsi] = (
-#         df[HeatmapColumn.cost_nonsi] / min_cost_spec
-#     )
-#     df[HeatmapColumn.min_cost_baseline] = min_cost_baseline
-#     df[HeatmapColumn.min_speedup_dsi_vs_baseline] = min_cost_baseline / min_cost_fed
-#     return df.reset_index()
-
 
 def get_enriched_min_speedups(df: pd.DataFrame) -> pd.DataFrame:
     # Setting index only once to be used throughout the function
