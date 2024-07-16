5238248311835b153b6a8c8a850550388342fe14
Korat, Daniel <daniel.korat@intel.com>
2024-07-16 01:03:04 -0700

    resolve merge conflits

--- dsi/types/exception.py
+++ dsi/types/exception.py
@@ -22,12 +22,37 @@ class DrafterSlowerThanTargetError(Exception):
         super().__init__(prefix + msg)
 
 
-class IncompatibleAppendError(Exception):
+class MissingHeatmapColumnError(Exception):
+    def __init__(self, column: str):
+        msg: str = (
+            "Missing required column in DataFrame."
+            f" Column '{column}' is required for the operation."
+        )
+        super().__init__(msg)
+
+
+class IncompatibleExtendError(Exception):
     def __init__(self, source_type: str, target_type: str):
         prefix: str = "Attempt to append data from incompatible types. "
         msg: str = (
             f"{prefix}Append operation requires both objects to be of the same type. "
             f"Source type: {source_type}, Target type: {target_type}."
+        )
+        super().__init__(msg)
+
+
+class HeatmapConfigInvalidAcceptanceRateRangeError(Exception):
+    def __init__(self, msg: str):
+        prefix: str = "Invalid acceptance rate range. "
+        super().__init__(prefix + msg)
+
+
+class ConfigPlotHeatmapInvalidLevelsStepError(Exception):
+    def __init__(self, levels_step: float):
+        msg: str = (
+            "Invalid levels step. Levels step must be a factor of 1."
+            f" Received levels step: {levels_step}, so that {1/levels_step=}"
+            " is not an integer."
         )
         super().__init__(msg)
 
