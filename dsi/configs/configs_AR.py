1d4e438b3724072b95c48e5790e80e60ce876363
jmamou <jonathan.mamou@intel.com>
2024-07-18 05:30:24 -0700

    minor

--- dsi/configs/configs_AR.py
+++ dsi/configs/configs_AR.py
@@ -4,14 +4,15 @@ class ConfigAcceptanteRate(BaseModel):
     """Includes all the parameters needed for measuring the acceptance rate 
     of a (target, draft, dataset) triplet.
     """
-    seed: int = Field(42, title="The random seed for each experiment")
-    num_ex: int = Field(5, title="The number of examples per dataset", ge=1)
+    seed: int = Field(42, 
+                      title="The random seed for each experiment")
+    num_ex: int = Field(5, 
+                        title="The number of examples per dataset", ge=1)
     max_new_tokens: int = Field(256, 
                                 title="The maximum number of new tokens to generate", 
                                 ge=1)
     compiled_model: bool = Field(False,
-                                 title="Whether to torch.compile() the model",
-                                 ge=1)
+                                 title="Whether to torch.compile() the model")
     do_sample_target: bool = Field(False,
                             title="whether enable sampling decoding strategies for the target model")
     do_sample_draft: bool = Field(False,
@@ -21,4 +22,4 @@ class ConfigAcceptanteRate(BaseModel):
                         ge=0)
     temp_draft: int = Field(1,
                         title="temperature for the draft model", 
-                        ge=0))
+                        ge=0)
