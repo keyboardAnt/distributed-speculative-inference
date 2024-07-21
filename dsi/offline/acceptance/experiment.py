d2da6c0e346b7323393dec5f7b1a15ab00e50a5d
jmamou <jonathan.mamou@intel.com>
2024-07-21 01:38:14 -0700

    formatting

--- dsi/offline/acceptance/experiment.py
+++ dsi/offline/acceptance/experiment.py
@@ -16,14 +16,17 @@ class ExperimentAcceptanceRate(ExperimentLatency):
     """
     Measures the generation acceptance rate.
     """
-    def __init__(self, 
-                 config: ConfigAcceptanteRate,
-                 gen_config: ConfigGen,
-                 draft_gen_config: ConfigGen):
+
+    def __init__(
+        self,
+        config: ConfigAcceptanteRate,
+        gen_config: ConfigGen,
+        draft_gen_config: ConfigGen,
+    ):
         self.config: ConfigAcceptanteRate
         super().__init__(config, gen_config)
         self.draft_gen_config: ConfigGen = draft_gen_config
-    
+
     def _load_draft_model(self) -> tuple:
         log.info(
             f"Loading model: {self.config.draft_model}, \
@@ -37,9 +40,8 @@ class ExperimentAcceptanceRate(ExperimentLatency):
         )
         model = torch.compile(model) if self.config.draft_compile_model else model
         return model
-     
+
     def run(self) -> float:
-        
         all_n_matches = []
 
         examples = self._get_random_prompted_examples()
@@ -51,15 +53,17 @@ class ExperimentAcceptanceRate(ExperimentLatency):
             temperature=self.gen_config.temperature,
             top_p=self.gen_config.top_p,
             pad_token_id=tokenizer.eos_token_id,
-            max_new_tokens=self.config.max_new_tokens
+            max_new_tokens=self.config.max_new_tokens,
         )
-              
+
         draft_model = self._load_draft_model()
-        draft_gen_kwargs = dict(do_sample=self.draft_gen_config.do_sample,
+        draft_gen_kwargs = dict(
+            do_sample=self.draft_gen_config.do_sample,
             temperature=self.draft_gen_config.temperature,
             top_p=self.draft_gen_config.top_p,
             pad_token_id=tokenizer.eos_token_id,
-            max_new_tokens=1)
+            max_new_tokens=1,
+        )
 
         for ex in tqdm(examples):
             inputs = tokenizer(ex, return_tensors="pt").to(target_model.device)
@@ -68,32 +72,40 @@ class ExperimentAcceptanceRate(ExperimentLatency):
             prompt_len = len(inputs.input_ids[0])
 
             for i in range(prompt_len, len(output_target[0])):
-                inputs['input_ids'] = output_target[0,0:i].view(1, -1)
-                inputs['attention_mask'] = torch.tensor([[1] * i], 
-                                                        device=draft_model.device)
+                inputs["input_ids"] = output_target[0, 0:i].view(1, -1)
+                inputs["attention_mask"] = torch.tensor(
+                    [[1] * i], device=draft_model.device
+                )
                 output_draft = draft_model.generate(**inputs, **draft_gen_kwargs)
                 if output_draft[-1, i] == output_target[-1, i]:
                     n_matches[-1] += 1
-                elif i <  len(output_target[0]) - 1: # new window
+                elif i < len(output_target[0]) - 1:  # new window
                     n_matches.append(0)
-                else: # at the end, remove last window
+                else:  # at the end, remove last window
                     n_matches.pop()
             all_n_matches += n_matches
         print(f"{all_n_matches=}")
-        ar = 1 - ( 1 / (1+np.array(all_n_matches).mean()))
-        print(f'Acceptance Rate : {round(ar*100,2)}')
+        ar = 1 - (1 / (1 + np.array(all_n_matches).mean()))
+        print(f"Acceptance Rate : {round(ar*100,2)}")
         return ar
 
+
 def main():
-    ar_config = ConfigAcceptanteRate(model="lmsys/vicuna-7b-v1.3",
-                                draft_model="double7/vicuna-68m", 
-                                dataset="cnn_dailymail", subset="2.0.0")
+    ar_config = ConfigAcceptanteRate(
+        model="lmsys/vicuna-7b-v1.3",
+        draft_model="double7/vicuna-68m",
+        dataset="cnn_dailymail",
+        subset="2.0.0",
+    )
     target_gen_config = ConfigGen(do_sample=False, temperature=1.0, top_p=1.0)
     draft_gen_config = ConfigGen(do_sample=False, temperature=1.0, top_p=1.0)
-    mar = ExperimentAcceptanceRate(config=ar_config, 
-                                   gen_config=target_gen_config, 
-                                   draft_gen_config=draft_gen_config)
+    mar = ExperimentAcceptanceRate(
+        config=ar_config,
+        gen_config=target_gen_config,
+        draft_gen_config=draft_gen_config,
+    )
     mar.run()
+
 
 if __name__ == "__main__":
     main()
