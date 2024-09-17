b4941da94b6bc152efd77d68df2645fc801ba85e
Nadav Timor <nadav.timor@weizmann.ac.il>
2024-09-17 17:55:55 +0000

    POC llama3.1 70b on 3 gpus

--- poc/dsi.py
+++ poc/dsi.py
@@ -4,6 +4,7 @@ from functools import cache
 import gc
 import os
 import threading
+import json
 import time
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -844,6 +845,12 @@ def setup_hf_cache():
     )
 
 
+def load_device_map(file_name):
+    with open(file_name, "r") as f:
+        device_map = json.load(f)
+    return device_map
+
+
 async def run(
     verifier_name: str,
     drafter_name: str,
@@ -867,10 +874,10 @@ async def run(
     # Define the missing arguments
     print(f"Loading tokenizer for {verifier_name}")
     print_gpu_memory()
-    # print("Main: Creating manager")
-    # manager = Manager(
-    print("Main: Creating ManagerSequential")
-    manager = ManagerSequential(
+    print("Main: Creating manager")
+    manager = Manager(
+    # print("Main: Creating ManagerSequential")
+    # manager = ManagerSequential(
         draft_queue,
         verify_queue,
         response_queue,
@@ -897,13 +904,16 @@ async def run(
     # visible_devices = ",".join(str(i) for i in range(1, torch.cuda.device_count()))
     # os.environ["CUDA_VISIBLE_DEVICES"] = visible_devices
     # print(f"Main: CUDA_VISIBLE_DEVICES set to {os.environ['CUDA_VISIBLE_DEVICES']}")
+    verifier_device_map = load_device_map("/workspace/distributed-speculative-inference/poc/device_map_meta-llama_Meta-Llama-3.1-70B-Instruct_8bit_on_3A40_auto.json")
+    print(f"Main: Verifier device map: {verifier_device_map}")
     await asyncio.gather(
         *[
             verifier.load_model(
                 verifier_name,
                 dtype=verifier_dtype,
                 # device_map="auto",
-                device_map="balanced_low_0",
+                # device_map="balanced_low_0",
+                device_map=verifier_device_map,
                 load_in_8bit=verifier_load_in_8bit,
                 cache_dir=os.environ["TRANSFORMERS_CACHE"],
             )
@@ -1042,16 +1052,12 @@ async def main():
     vocab_size: int = 128256
     lookahead: int = 10
     max_new_tokens: int = 100
-    prompt: str = """Below is an instruction that describes a
-task, paired with an input that provides
-further context. Write a response that
-appropriately completes the request.
+    prompt: str = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 ### Instruction:
-Produce an annotated bibliography for the provided paper.
+Break the text into two logical paragraphs.
 ### Input:
-Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request. ### Instruction: Produce an annotated bibliography for the provided paper. ### Input: The provided paper is an article about the impact of social media on mental health titled “Exploring the Impact of Social Media on Mental Health”. ### Response: Chou, S., S. C. Y., & Ioerger, M. (2019). Exploring the Impact of Social Media on Mental Health. International Journal of Mental Health, 32(2). This paper discusses the impacts of different kinds of social media on the mental health of users. Firstly, the paper uses a systematic review to explore the existing research on the link between social media use and mental health. The review provides an overview of the current state of the research and discusses both positive and negative impacts that social media can have. Results suggest that there is both a positive and a negative correlation between the use of social media and mental health. The authors then proceed to focus on the effects of four specific types of social media: Instagram, Facebook, Twitter and Youtube. For each type of social media, the authors explore the effects on three specific mental health domains: depression, anxiety, and self-esteem. The paper concludes that more research is needed to understand the relationship between social media and mental health. Bates, M., & LeMesurier, S. (2017). The effect of social media on mental health. Mental Illness, 5(1). This paper examines the potential effects of social media on aspects of mental health such as self-esteem, depression, and anxiety. The authors use an online survey of 644 participants to investigate the relationship between mental health and particular uses of Facebook and Instagram. The survey looks at different users' motives for using social media, the frequency with which they use it, feelings of loneliness or anxiety while using it, and how their real life values and perspectives are impacted by social media. Results indicate that several factors influence the relationship between social media and mental health, including positive and negative attributes of different social media types. The findings of the paper suggest that research into what might influence how users engage with social media and the particular effects of platforms and ways of using them could be beneficial to understanding the relationship between social media and mental health. Olah, Z., Z. Szatmári, & Font, S. (2019). Effects of Social Media on Mental Health. Frontiers in Psychology, 10(2). The authors of this paper explore the potential effects of social media on mental health. The paper highlights both positive and negative outcomes from the use of different types of social media. It also highlights the ways in which our mental health is inextricably linked to our social life and environment. Results from a systematic review suggest that different types of social media have different effects on individuals. For example, it found that social media use has a positive effect on collaboration, connectedness, and communication, while it can also have a negative effect on loneliness, anxiety, depression and self-esteem. The paper concludes that more research is needed to understand how these different types of social media affect our mental wellbeing.
-### Response:
-"""
+The meetings can be live or virtual, but with the pandemic continuing in many parts of the world, many companies will opt for virtual meetings in order to minimize the spread of illness. Virtual meetings also bring an array of advantages like being able to connect with people in different parts of the world.
+### Response:"""
     tok_ids = encode(prompt, verifier_name)
     # tok_ids = generate(
     #     model_name=verifier_name,
